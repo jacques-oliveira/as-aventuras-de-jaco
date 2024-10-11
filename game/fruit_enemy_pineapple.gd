3f7bee636a53bb2ff0311912a7e1762128e9e2d2
jacques-oliveira <jacques.oliveira.eng@gmail.com>
2024-10-11 17:27:58 -0300

    inimigos ativos

--- game/fruit_enemy_pineapple.gd
+++ game/fruit_enemy_pineapple.gd
@@ -1,5 +1,29 @@
-extends CharacterBody2D
+extends RigidBody2D
+var speed = 100
+var player_chase = false
+var player = null
 
-
+func _ready() -> void:
+	$AnimatedSprite2D.play("idle")
+	
+func _physics_process(delta: float) -> void:
+	if player_chase && player.is_in_group("player") :
+		position += (player.position - position)/speed
+		$AnimatedSprite2D.play("walk")
+		if (player.position.x - position.x) < 0:
+			$AnimatedSprite2D.flip_h = true
+		else:
+			$AnimatedSprite2D.flip_h = false
+	else:
+		$AnimatedSprite2D.play("idle")
+		
 func _on_detection_area_body_entered(body: Node2D) -> void:
-	pass # Replace with function body.
+	if body.is_in_group("player"):
+		player = body
+		player_chase = true
+
+func _on_detection_area_body_exited(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		await get_tree().create_timer(2.5).timeout
+		player = null
+		player_chase = false	
--- game/player.gd
+++ game/player.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-const SPEED = 300.0
+const SPEED = 325.0
 @export var maxHealth = 3
 @onready var currentHealth: int = maxHealth
 
