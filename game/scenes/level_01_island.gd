227247d86ca13ca34a5bfb4b845c5ef7e6aa96dc
jacques-oliveira <jacques.oliveira.eng@gmail.com>
2024-10-13 19:15:56 -0300

    lifeCount ui

--- game/scenes/level_01_island.gd
+++ game/scenes/level_01_island.gd
@@ -1,8 +1,11 @@
 extends Node2D
 @onready var heartsContainer = $TileMap/CanvasLayer/heartsContainer
 @onready var player = $TileMap/Player
+@onready var lifeCount = 3
+@onready var lifeLabel = $TileMap/CanvasLayer/HBoxContainer/LifeCount
 
 func _ready() -> void:
+	lifeLabel.text = "X" + str(lifeCount)
 	heartsContainer.setMaxHearts(player.maxHealth)
 	heartsContainer.updateHearts(player.currentHealth)
 	player.healthChanged.connect(heartsContainer.updateHearts)
