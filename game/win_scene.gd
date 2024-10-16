5084240f99e9e1b9744f334d86e48e69692540d5
jacques-oliveira <jacques.oliveira.eng@gmail.com>
2024-10-16 00:27:18 -0300

    ajuste win scene

--- game/win_scene.gd
+++ game/win_scene.gd
@@ -1,6 +1,14 @@
 extends Control
 
 @onready var coinScoreLabel = $MarginContainer/VBoxContainer/CanvasLayer/CoinGUI/labelCointCount
+@onready var particle = $GPUParticles2D
 
 func _ready() -> void:
 	coinScoreLabel.text = "X" + str(GameManager.cointCount)
+	particle.emitting = true
+	particle.one_shot = true
+	particle.restart()
+
+func _input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("ui_accept"):
+		get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
